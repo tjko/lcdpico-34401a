70f19976373d7f2ecc03ca92e17afc308892c798
Timo Kokkonen <tjko@iki.fi>
2026-08-03 22:31:51 -0700

    remove obsolete hack

--- build_httpd_fs.sh
+++ build_httpd_fs.sh
@@ -19,7 +19,5 @@ fatal() { echo "`basename $0`: $*"; exit 1; }
 dos2unix ${FSDATAFILE}
 [ $? -eq 0 ] || fatal "dos2unix failed"
 
-# HACK: make sure next build will recompile fanpico_fsdata.c...
-find build/ -type f -name 'fs.c.obj' -print -delete
 
 
