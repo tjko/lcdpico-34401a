748681fab0d768ebb1cd96dc738c78006ddd0264
Timo Kokkonen <tjko@iki.fi>
2026-08-03 22:10:24 -0700

    add helper scripts

---
+++ build_httpd_fs.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# build_httpd_fs.sh
+#
+
+SERVER="LcdPico (https://github.com/tjko/lcd-pico-34401a)"
+
+FSDIR=src/httpd-fs/
+FSDATAFILE=src/lcdpico_fsdata.c
+
+fatal() { echo "`basename $0`: $*"; exit 1; }
+
+[ -d "$FSDIR" ] || fatal "cannot find fs directory: $FSDIR"
+
+
+./contrib/makefsdata ${FSDIR} -m -svr:"${SERVER}" -ssi:src/httpd-fs_ssi.list -f:${FSDATAFILE} -x:html~,shtml~,json~,~
+[ $? -eq 0 ] || fatal "makefsdata failed"
+
+dos2unix ${FSDATAFILE}
+[ $? -eq 0 ] || fatal "dos2unix failed"
+
+# HACK: make sure next build will recompile fanpico_fsdata.c...
+find build/ -type f -name 'fs.c.obj' -print -delete
+
+
